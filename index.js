eec88a92a2d95596f57dd622996578481f590376
Iván Rodríguez Murillo <wantez@gmail.com>
2016-12-10 00:39:19 +0100

    common

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const redis = require('./redis')();
+const redis = require('weplay-common').redis();
 const io = require('socket.io-emitter')(redis);
 const interval = process.env.WEPLAY_INTERVAL || 5000;
+const logger = require('weplay-common').logger('weplay-presence');
 
 setInterval(() => {
     redis.hgetall('weplay:connections', (err, counts) => {
         if (!counts) return;
         let count = 0;
         for (const i in counts) count += Number(counts[i]);
+        logger.info('connections', count);
         redis.set('weplay:connections-total', count);
         io.emit('connections', count);
     });
